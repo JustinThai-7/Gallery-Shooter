fbbec9de14b3d49369b32ed9a2d597db25e8ed08
JustinThai-7 <justinthai.x@gmail.com>
2025-06-09 21:35:46 -0700

    menu fix

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-    scene: [MainMenu, Shooter, Controls, Credits, GameOver, HighScore]
-};
+window.onload = function() {
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 0 },
+                debug: false
+            }
+        },
+        scene: [MainMenu, Shooter, Controls, Credits, GameOver, HighScore]
+    };
 
-const game = new Phaser.Game(config);
+    const game = new Phaser.Game(config);
+};
